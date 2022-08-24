d76075760eb6f826e9f1168e14e969ff2e5151bf
Arial-Z <amonitux@outlook.com>
2022-08-25 00:25:11 +0200

    working on it

--- movie-renamer.sh
+++ movie-renamer.sh
@@ -2,7 +2,7 @@
 
 # function
 function get-mal-id () {
-jq ".[] | select(".tvdb_id"==${tvdb_id})" -r $SCRIPT_FOLDER/pmm_anime_ids.json |jq ."mal_id" | sort -n | head -1
+jq '.[] | select(.imdb_id == "${imdb_id}" )' -r $SCRIPT_FOLDER/pmm_anime_ids.json |jq ."mal_id" | sort -n | head -1
 }
 function get-mal-infos () {
 wget "https://api.jikan.moe/v4/anime/$mal_id" -O $SCRIPT_FOLDER/infos/$mal_id.json 
@@ -23,20 +23,21 @@ sleep 2
 ## folder and file emplacement
 SCRIPT_FOLDER=/home/arialz/github/Plex-Animes-Renamer
 PMM_FOLDER=/home/plexmetamanager
-LOG_PATH=/home/arialz/log/plex-renamer_$(date +%Y.%m.%d).log
-animes_titles=$PMM_FOLDER/config/animes/animes-titles.yml
+LOG_PATH=/home/arialz/log/movies-renamer_$(date +%Y.%m.%d).log
+animes_titles=$PMM_FOLDER/config/animes/movies-titles.yml
 
 # get library titles and tvdb-ID list by PMM
-rm $PMM_FOLDER/config/temp-animes.cache
-rm $SCRIPT_FOLDER/animes.csv
-$PMM_FOLDER/pmm-venv/bin/python3 $PMM_FOLDER/plex_meta_manager.py -r --config $PMM_FOLDER/config/temp-animes.yml
+rm $PMM_FOLDER/config/temp-movies.cache
+rm $SCRIPT_FOLDER/movie.csv
+cd $PMM_FOLDER
+$PMM_FOLDER/pmm-venv/bin/python3 $PMM_FOLDER/plex_meta_manager.py -r --config $PMM_FOLDER/config/temp-movies.yml
 mv $PMM_FOLDER/config/logs/meta.log $SCRIPT_FOLDER
-line_start=$(grep -n "Mapping Animes Library" $SCRIPT_FOLDER/meta.log | cut -d : -f 1)
+line_start=$(grep -n "Mapping Animes Films Library" $SCRIPT_FOLDER/meta.log | cut -d : -f 1)
 line_end=$(grep -n "Running animes-airing Metadata File" $SCRIPT_FOLDER/meta.log | cut -d : -f 1)
-head -n $line_end $SCRIPT_FOLDER/meta.log | tail -n $(( $line_end -$line_start - 1 )) | head -n -5 > $SCRIPT_FOLDER/cleanlog.txt
+head -n $line_end $SCRIPT_FOLDER/meta.log | tail -n $(( $line_end -$line_start - 1 )) | head -n -5 > $SCRIPT_FOLDER/movies-cleanlog.txt
 rm $SCRIPT_FOLDER/meta.log
-awk -F"|" '{ OFS = "|" } ; { gsub(/ /,"",$5) } ; { print substr($5,8),substr($7,2,length($7)-2) }' $SCRIPT_FOLDER/cleanlog.txt > $SCRIPT_FOLDER/list-animes.csv
-rm $SCRIPT_FOLDER/cleanlog.txt
+awk -F"|" '{ OFS = "|" } ; { gsub(/ /,"",$6) } ; { print substr($6,8),substr($7,2,length($7)-2) }' $SCRIPT_FOLDER/movies-cleanlog.txt > $SCRIPT_FOLDER/list-movies.csv
+rm $SCRIPT_FOLDER/movies-cleanlog.txt
 curl "https://raw.githubusercontent.com/meisnate12/Plex-Meta-Manager-Anime-IDs/master/pmm_anime_ids.json" > $SCRIPT_FOLDER/pmm_anime_ids.json # get pmm animes mapping
 if [ ! -f $animes_titles ] # check if $animes_titles exist
 then
@@ -54,40 +55,40 @@ then
 fi
 
 # get corresponding MAL title and score
-while IFS="|" read -r tvdb_id title_plex
+while IFS="|" read -r imdb_id title_plex
 do
-        if [ -f $SCRIPT_FOLDER/ID-animes.csv ] # check if ID-animes.csv exist
+        if [ -f $SCRIPT_FOLDER/ID-movies.csv ] # check if ID-animes.csv exist
         then
-                if ! awk -F"|" '{print $1}' $SCRIPT_FOLDER/ID-animes.csv | grep $tvdb_id
+                if ! awk -F"|" '{print $1}' $SCRIPT_FOLDER/ID-movies.csv | grep $imdb_id
                 then
-                        if awk -F"|" '{print $1}' $SCRIPT_FOLDER/override-ID-animes.csv | tail -n +2 | grep $tvdb_id                   # check if in override
+                        if awk -F"|" '{print $1}' $SCRIPT_FOLDER/override-ID-movies.csv | tail -n +2 | grep $imdb_id                   # check if in override
                         then
-                                overrideline=$(grep -n "$tvdb_id" $SCRIPT_FOLDER/override-ID-animes.csv | cut -d : -f 1)
-                                mal_id=$(sed -n "${overrideline}p" $SCRIPT_FOLDER/override-ID-animes.csv | awk -F"|" '{print $2}')
-                                title_mal=$(sed -n "${overrideline}p" $SCRIPT_FOLDER/override-ID-animes.csv | awk -F"|" '{print $3}')
+                                overrideline=$(grep -n "$imdb_id" $SCRIPT_FOLDER/override-ID-movies.csv | cut -d : -f 1)
+                                mal_id=$(sed -n "${overrideline}p" $SCRIPT_FOLDER/override-ID-movies.csv | awk -F"|" '{print $2}')
+                                title_mal=$(sed -n "${overrideline}p" $SCRIPT_FOLDER/override-ID-movies.csv | awk -F"|" '{print $3}')
                                 get-mal-infos
 				echo "override found for : $title_mal / $title_plex" >> $LOG_PATH
-                                echo "$tvdb_id|$mal_id|$title_mal|$title_plex" >> $SCRIPT_FOLDER/ID-animes.csv
+                                echo "$imdb_id|$mal_id|$title_mal|$title_plex" >> $SCRIPT_FOLDER/ID-movies.csv
 			else
                                 mal_id=$(get-mal-id)
                                 if [[ "$mal_title" == 'null' ]] || [[ "$mal_id" == 'null' ]] || [[ "${#mal_id}" == '0' ]]
                                 then
-                                        echo "invalid MAL ID for : tvdb : $tvdb_id / $title_plex" >> $LOG_PATH
+                                        echo "invalid MAL ID for : imdb : $imdb_id / $title_plex" >> $LOG_PATH
                                 fi
 				get-mal-infos
                                 title_mal=$(get-mal-title)
-                                echo "$tvdb_id|$mal_id|$title_mal|$title_plex" >> $SCRIPT_FOLDER/ID-animes.csv
+                                echo "$imdb_id|$mal_id|$title_mal|$title_plex" >> $SCRIPT_FOLDER/ID-movies.csv
                         fi
                 fi
         else
-                if awk -F"|" '{print $1}' $SCRIPT_FOLDER/override-ID-animes.csv | tail -n +2 | grep $tvdb_id                   # check if in override
+                if awk -F"|" '{print $1}' $SCRIPT_FOLDER/override-ID-movies.csv | tail -n +2 | grep $imdb_id                   # check if in override
                 then
-                        overrideline=$(grep -n "$tvdb_id" $SCRIPT_FOLDER/override-ID-animes.csv | cut -d : -f 1)
-                        mal_id=$(sed -n "${overrideline}p" $SCRIPT_FOLDER/override-ID-animes.csv | awk -F"|" '{print $2}')
-                        title_mal=$(sed -n "${overrideline}p" $SCRIPT_FOLDER/override-ID-animes.csv | awk -F"|" '{print $3}')
+                        overrideline=$(grep -n "$imdb_id" $SCRIPT_FOLDER/override-ID-movies.csv | cut -d : -f 1)
+                        mal_id=$(sed -n "${overrideline}p" $SCRIPT_FOLDER/override-ID-movies.csv | awk -F"|" '{print $2}')
+                        title_mal=$(sed -n "${overrideline}p" $SCRIPT_FOLDER/override-ID-movies.csv | awk -F"|" '{print $3}')
                         get-mal-infos
 			echo "override found for : $title_mal / $title_plex" >> $LOG_PATH
-                        echo "$tvdb_id|$mal_id|$title_mal|$title_plex" >> $SCRIPT_FOLDER/ID-animes.csv
+                        echo "$imdb_id|$mal_id|$title_mal|$title_plex" >> $SCRIPT_FOLDER/ID-movies.csv
                 else
                         mal_id=$(get-mal-id)
                         if [[ "$mal_title" == 'null' ]] || [[ "$mal_id" == 'null' ]] || [[ "${#mal_id}" == '0' ]]
@@ -99,7 +100,8 @@ do
                         echo "$tvdb_id|$mal_id|$title_mal|$title_plex" >> $SCRIPT_FOLDER/ID-animes.csv
                 fi
         fi
-done < $SCRIPT_FOLDER/list-animes.csv
+done < $SCRIPT_FOLDER/list-movies.csv
+sleep 30
 
 # write PMM metadata file
 while IFS="|" read -r tvdb_id mal_id title_mal title_plex
