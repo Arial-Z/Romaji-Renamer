6128f6ec7aa39c501da415f3ed564efde7e66087
Arial-Z <amonitux@outlook.com>
2022-09-03 19:11:26 +0200

    Update animes-renamer.sh

--- animes-renamer.sh
+++ animes-renamer.sh
@@ -203,7 +203,7 @@ do
 		if sed -n "${labelline}p" $animes_titles | grep "label"			# replace the Ongoing label according to MAL airing list
 		then
 			sed -i "${labelline}d" $animes_titles
-			if [[ awk -F"\t" '{print "\""$3"\":"}' $SCRIPT_FOLDER/data/ongoing.tsv | grep -w "\"$title_mal\":" && awk -F"\t" '{print "\""$2"\":"}' $SCRIPT_FOLDER/data/top-animes-100.tsv | grep -w "\"$title_mal\":" ]];
+			if [[ awk -F"\t" '{print "\""$3"\":"}' $SCRIPT_FOLDER/data/ongoing.tsv | grep -w "\"${title_mal}\":" && awk -F"\t" '{print "\""$2"\":"}' $SCRIPT_FOLDER/data/top-animes-100.tsv | grep -w "\"${title_mal}\":" ]];
 			then
 				sed -i "${labelline}i\    label: Ongoing, A-100" $animes_titles
 				echo "$(date +%Y.%m.%d" - "%H:%M:%S) - $title_mal added to Ongoing, A-100" >> $LOG
