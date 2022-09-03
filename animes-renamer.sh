f2ecfd1360cd8e7a0136a7a6c62926da54b0883d
Arial-Z <amonitux@outlook.com>
2022-09-03 18:58:21 +0200

    Update animes-renamer.sh

--- animes-renamer.sh
+++ animes-renamer.sh
@@ -203,11 +203,11 @@ do
 		if sed -n "${labelline}p" $animes_titles | grep "label"			# replace the Ongoing label according to MAL airing list
 		then
 			sed -i "${labelline}d" $animes_titles
-			if  awk -F"\t" '{print "\""$3"\":"}' $SCRIPT_FOLDER/data/ongoing.tsv | grep "\"$title_mal\":" && awk -F"\t" '{print "\""$2"\":"}' $SCRIPT_FOLDER/data/top-animes-100.tsv | grep "\"$title_mal\":"
+			if  [awk -F"\t" '{print "\""$3"\":"}' $SCRIPT_FOLDER/data/ongoing.tsv | grep "\"$title_mal\":" && awk -F"\t" '{print "\""$2"\":"}' $SCRIPT_FOLDER/data/top-animes-100.tsv | grep "\"$title_mal\":" ];
 			then
 				sed -i "${labelline}i\    label: Ongoing, A-100" $animes_titles
 				echo "$(date +%Y.%m.%d" - "%H:%M:%S) - $title_mal added to Ongoing, A-100" >> $LOG
-			elif  awk -F"\t" '{print "\""$3"\":"}' $SCRIPT_FOLDER/data/ongoing.tsv | grep "\"$title_mal\":" && awk -F"\t" '{print "\""$2"\":"}' $SCRIPT_FOLDER/data/top-animes-250.tsv | grep "\"$title_mal\":"
+			elif  [ awk -F"\t" '{print "\""$3"\":"}' $SCRIPT_FOLDER/data/ongoing.tsv | grep "\"$title_mal\":" && awk -F"\t" '{print "\""$2"\":"}' $SCRIPT_FOLDER/data/top-animes-250.tsv | grep "\"$title_mal\":" ];
 			then
 				sed -i "${labelline}i\    label: Ongoing, A-100" $animes_titles
 				echo "$(date +%Y.%m.%d" - "%H:%M:%S) - $title_mal added to Ongoing, A-250" >> $LOG
