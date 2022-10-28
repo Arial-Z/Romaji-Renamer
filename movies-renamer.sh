4a8ae21dda477856b2acd12af1b61b837e0e8751
Arial-Z <amonitux@outlook.com>
2022-10-28 15:14:22 +0200

    code cleanup

--- movies-renamer.sh
+++ movies-renamer.sh
@@ -46,6 +46,9 @@ jq '.data.studios[0] | [.name]| @tsv' -r $SCRIPT_FOLDER/data/movies/$mal_id.json
 if [ ! -f $movies_titles ]
 then
         echo "metadata:" > $movies_titles
+else
+	rm $movies_titles
+	echo "metadata:" > $movies_titles
 fi
 if [ ! -d $SCRIPT_FOLDER/data ]											#check if exist and create folder for json data
 then
@@ -69,6 +72,9 @@ then
 	touch $SCRIPT_FOLDER/ID/movies.tsv
 elif [ ! -f $SCRIPT_FOLDER/ID/movies.tsv ]
 then
+	touch $SCRIPT_FOLDER/ID/movies.tsv
+else
+	rm $SCRIPT_FOLDER/ID/movies.tsv
 	touch $SCRIPT_FOLDER/ID/movies.tsv	
 fi
 if [ ! -d $SCRIPT_FOLDER/tmp ]
@@ -99,21 +105,6 @@ line_start=$(grep -n "Mapping Animes Films Library" $SCRIPT_FOLDER/tmp/meta.log 
 line_end=$(grep -n -m1 "Animes Films Library Operations" $SCRIPT_FOLDER/tmp/meta.log | cut -d : -f 1)
 head -n $line_end $SCRIPT_FOLDER/tmp/meta.log | tail -n $(( $line_end - $line_start - 1 )) | head -n -5 > $SCRIPT_FOLDER/tmp/cleanlog-movies.txt
 awk -F"|" '{ OFS = "\t" } ; { gsub(/ /,"",$6) } ; { print  substr($6,8),substr($7,2,length($7)-2) }' $SCRIPT_FOLDER/tmp/cleanlog-movies.txt > $SCRIPT_FOLDER/tmp/list-movies.tsv
-
-# Cleanup ID/movies.tsv
-printf "\nCleaning ID/animes.tsv\n"  >> $LOG 
-line=1
-while IFS=$'\t' read -r imdb_id mal_id title_mal title_plex
-do
-        if ! awk -F"\t" '{print $1}' $SCRIPT_FOLDER/tmp/list-movies.tsv | grep -w $imdb_id
-        then
-                sed -i "${line}d" $SCRIPT_FOLDER/ID/movies.tsv
-                echo "$(date +%Y.%m.%d" - "%H:%M:%S) - $title_mal removed from ID/movies.tsv"  >> $DELETED_LOG
-        else
-                ((line++))
-        fi
-done < $SCRIPT_FOLDER/ID/movies.tsv
-printf "ID/movies.tsv cleanup finished\n"  >> $LOG
 
 # create ID/movies.tsv ( imdb_id | mal_id | title_mal | title_plex )
 while IFS=$'\t' read -r imdb_id mal_id title_mal
@@ -146,67 +137,22 @@ done < $SCRIPT_FOLDER/tmp/list-movies.tsv
 # write PMM metadata file from ID/movies.tsv and jikan API
 while IFS=$'\t' read -r imdb_id mal_id title_mal title_plex
 do
-	if grep "\"$title_mal\":" $movies_titles
-	then
-		get-mal-infos
-		get-mal-poster
-		sorttitleline=$(grep -n "sort_title: \"$title_mal\"" $movies_titles | cut -d : -f 1)
-		ratingline=$((sorttitleline+1))
-		echo "$(date +%Y.%m.%d" - "%H:%M:%S) - $title_mal metadata updated :" >> $LOG
-		if sed -n "${ratingline}p" $movies_titles | grep "audience_rating:"
-		then
-			sed -i "${ratingline}d" $movies_titles
-			score_mal=$(get-mal-rating)
-			sed -i "${ratingline}i\    audience_rating: ${score_mal}" $movies_titles
-			printf "$(date +%Y.%m.%d" - "%H:%M:%S)\t\tscore : $score_mal\n" >> $LOG
-		fi
-		tagsline=$((sorttitleline+2))
-		if sed -n "${tagsline}p" $movies_titles | grep "genre.sync:"
-		then
-			sed -i "${tagsline}d" $movies_titles
-			mal_tags=$(get-mal-tags)
-			sed -i "${tagsline}i\    genre.sync: Anime,${mal_tags}" $movies_titles
-			printf "$(date +%Y.%m.%d" - "%H:%M:%S)\t\ttags updated : $mal_tags\n" >> $LOG
-		fi
-	else
-		get-mal-infos
-		echo "  \"$title_mal\":" >> $movies_titles
-                echo "    alt_title: \"$title_plex\"" >> $movies_titles
-                echo "    sort_title: \"$title_mal\"" >> $movies_titles
-		echo "$(date +%Y.%m.%d" - "%H:%M:%S) - $title_mal / $title_plex added to metadata :" >> $LOG
-		score_mal=$(get-mal-rating)
-                echo "    audience_rating: $score_mal" >> $movies_titles
-		printf "$(date +%Y.%m.%d" - "%H:%M:%S)\t\tscore : $score_mal\n" >> $LOG
-		mal_tags=$(get-mal-tags)
-		echo "    genre.sync: Anime,${mal_tags}"  >> $movies_titles
-		printf "$(date +%Y.%m.%d" - "%H:%M:%S)\t\ttags : $mal_tags\n" >> $LOG
-		mal_studios=$(get-mal-studios)
-		echo "    studio: ${mal_studios}"  >> $movies_titles
-		printf "$(date +%Y.%m.%d" - "%H:%M:%S)\t\tstudio : $mal_studios" >> $LOG
-		get-mal-poster
-		echo "    file_poster: $SCRIPT_FOLDER/posters/${mal_id}.jpg" >> $movies_titles
-		printf "$(date +%Y.%m.%d" - "%H:%M:%S)\t\tPoster added\n" >> $LOG
-		echo "$(date +%Y.%m.%d" - "%H:%M:%S) - added to metadata :\n\t$title_mal / $title_plex" >> $ADDED_LOG
-	fi
+	get-mal-infos
+	echo "  \"$title_mal\":" >> $movies_titles
+	echo "    alt_title: \"$title_plex\"" >> $movies_titles
+	echo "    sort_title: \"$title_mal\"" >> $movies_titles
+	echo "$(date +%Y.%m.%d" - "%H:%M:%S) - $title_mal / $title_plex added to metadata :" >> $LOG
+	score_mal=$(get-mal-rating)
+	echo "    audience_rating: $score_mal" >> $movies_titles
+	printf "$(date +%Y.%m.%d" - "%H:%M:%S)\t\tscore : $score_mal\n" >> $LOG
+	mal_tags=$(get-mal-tags)
+	echo "    genre.sync: Anime,${mal_tags}"  >> $movies_titles
+	printf "$(date +%Y.%m.%d" - "%H:%M:%S)\t\ttags : $mal_tags\n" >> $LOG
+	mal_studios=$(get-mal-studios)
+	echo "    studio: ${mal_studios}"  >> $movies_titles
+	printf "$(date +%Y.%m.%d" - "%H:%M:%S)\t\tstudio : $mal_studios" >> $LOG
+	get-mal-poster
+	echo "    file_poster: $SCRIPT_FOLDER/posters/${mal_id}.jpg" >> $movies_titles
+	printf "$(date +%Y.%m.%d" - "%H:%M:%S)\t\tPoster added\n" >> $LOG
+	echo "$(date +%Y.%m.%d" - "%H:%M:%S) - added to metadata :\n\t$title_mal / $title_plex" >> $ADDED_LOG
 done < $SCRIPT_FOLDER/ID/movies.tsv
-
-# Remove from metadata deleted animes
-printf "\nRunning metadata cleanup\n"  >> $LOG 
-sed '/sort_title:/!d'  $movies_titles > $SCRIPT_FOLDER/tmp/movies-title-metadata.txt
-line=1
-while read -r title_metadata
-do
-        if ! awk -F"\t" '{print "sort_title: \""$3"\""}' $SCRIPT_FOLDER/ID/movies.tsv | grep "${title_metadata}"
-        then
-                lineprevious=$((line - 1))
-                previoustitle=$(sed -n "${lineprevious}p" $SCRIPT_FOLDER/tmp/movies-title-metadata.txt)
-                lineprevioustitle=$(grep -n "${previoustitle}" $animes_titles | cut -d : -f 1)
-                linedelstart=$((lineprevioustitle + 6))
-                linedelend=$((lineprevioustitle + 13))
-                sed -i "${linedelstart},${linedelend}d" $movies_titles
-                title=$(echo $title_metadata | cut -c 14- | sed 's/.$//')
-                echo "$(date +%Y.%m.%d" - "%H:%M:%S) - removed from metadata :\n\t$title"  >> $DELETED_LOG
-        fi
-        ((line++))
-done < $SCRIPT_FOLDER/tmp/movies-title-metadata.txt
-printf "metadata cleanup finished\n"  >> $LOG
