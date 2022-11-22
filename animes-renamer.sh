be83b197916f8a193e5c0dda6f7a66e90a70f11b
Arial-Z <amonitux@outlook.com>
2022-11-22 19:09:27 +0100

    Update animes-renamer.sh

--- animes-renamer.sh
+++ animes-renamer.sh
@@ -185,24 +185,23 @@ then
 				echo "Ongoing invalid TVDB ID for : MAL : $mal_id" >> $LOG
 				continue
 			else    												# get the mal ID again but main anime and create ongoing list
-				mal_id=$(get-mal-id)
-				anilist_id=$(get-anilist-id)
 				if awk -F"\t" '{print $1}' $SCRIPT_FOLDER/override-ID-animes.tsv | grep -w  $tvdb_id
 				then
 					line=$(grep -w -n $tvdb_id $SCRIPT_FOLDER/override-ID-animes.tsv | cut -d : -f 1)
 					mal_id=$(sed -n "${line}p" $SCRIPT_FOLDER/override-ID-animes.tsv | awk -F"\t" '{print $2}')
 					title_anime=$(sed -n "${line}p" $SCRIPT_FOLDER/override-ID-animes.tsv | awk -F"\t" '{print $3}')
 					printf "$tvdb_id\t$mal_id\t$title_anime\n" >> $SCRIPT_FOLDER/data/animes/ongoing.tsv
-				elif [[ "$title_anime" == 'null' ]] || [[ "$mal_id" == 'null' ]] || [[ "${#mal_id}" == '0' ]]       # Ignore anime with no tvdb to mal id conversion show in the error log you need to add them by hand in override
-				then
-					echo "$(date +%Y.%m.%d" - "%H:%M:%S) - invalid MAL ID for Ongoing : tvdb : $tvdb_id" >> $LOG
-					continue
-				else
-					get-mal-infos
-					get-anilist-infos
-					title_anime=$(get-anilist-title)
-					printf "$tvdb_id\t$mal_id\t$title_anime\n" >> $SCRIPT_FOLDER/data/animes/ongoing.tsv
-				fi
+					mal_id=$(get-mal-id)
+					anilist_id=$(get-anilist-id)
+					elif [[ "$title_anime" == 'null' ]] || [[ "$mal_id" == 'null' ]] || [[ "${#mal_id}" == '0' ]]       # Ignore anime with no tvdb to mal id conversion show in the error log you need to add them by hand in override
+					then
+						echo "$(date +%Y.%m.%d" - "%H:%M:%S) - invalid MAL ID for Ongoing : tvdb : $tvdb_id" >> $LOG
+						continue
+					else
+						get-anilist-infos
+						title_anime=$(get-anilist-title)
+						printf "$tvdb_id\t$mal_id\t$title_anime\n" >> $SCRIPT_FOLDER/data/animes/ongoing.tsv
+					fi
 			fi
 		fi
         done < $SCRIPT_FOLDER/tmp/ongoing.tsv
