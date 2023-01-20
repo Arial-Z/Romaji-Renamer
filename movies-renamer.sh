65e2e1b510217687248e843852cd39af72cb4d30
Arial-Z <amonitux@outlook.com>
2023-01-20 19:37:14 +0100

    fix docker permission

--- movies-renamer.sh
+++ movies-renamer.sh
@@ -141,6 +141,7 @@ then
 	cp $PMM_FOLDER_CONFIG/logs/meta.log $SCRIPT_FOLDER/tmp
 elif [ "$PMM_INSTALL_TYPE"  == "docker" ]
 then
+	docker exec -it $DOCKER_CONTAINER_NAME chmod 777 config/temp-movies.cache
 	docker exec -it $DOCKER_CONTAINER_NAME rm config/temp-movies.cache
 	docker exec -it $DOCKER_CONTAINER_NAME python plex_meta_manager.py -r --config config/temp-movies.yml
 	docker exec -it $DOCKER_CONTAINER_NAME chmod 777 config/logs/meta.log
