acc66aa95ec62bb61c417d7f9b44a0927fccdd67
Arial-Z <amonitux@outlook.com>
2023-02-09 22:40:52 +0100

    Fix log

--- functions.sh
+++ functions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #General variables
-LOG=$LOG_FOLDER/$TYOE_$(date +%Y.%m.%d).log
+LOG=$LOG_FOLDER/$media_type_$(date +%Y.%m.%d).log
 MATCH_LOG=$LOG_FOLDER/missing-id.log
 
 # functions
