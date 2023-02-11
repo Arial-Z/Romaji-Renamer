1bf050d600ade8de69244bddb6acaf1bf9e2d727
Arial-Z <amonitux@outlook.com>
2023-02-11 02:35:40 +0100

    export list from plexapi

---
+++ plex_animes_export.py
@@ -0,0 +1,28 @@
+from plexapi.server import PlexServer
+from dotenv import load_dotenv
+from os import environ, path
+import re
+import pathlib
+
+# Find .env file
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+# General Config
+url = environ.get('plex_url')
+token = environ.get('plex_token')
+ANIME_LIBRARY_NAME=environ.get('ANIME_LIBRARY_NAME')
+
+plex = PlexServer(url, token)
+animes = plex.library.section(ANIME_LIBRARY_NAME)
+with open("tmp/plex_animes_export.tsv", "w") as export_plex:
+        for video in animes.search():
+                title = str(video.title)
+                ids = str(video.guids)
+                tvdb = re.search("(?<=tvdb://)(\d+)", ids).group()
+                location = str(video.locations)[2:-2]
+                path = pathlib.PurePath(location)
+                folder = str(path.name)
+                season = str(video.childCount)
+                export=(tvdb + "\t" + title + "\t" + folder + "\t"+ season + "\n")
+                export_plex.write(export)
