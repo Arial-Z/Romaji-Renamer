8afd689ff1cc60e7a5b9969208e363fcecd8c3d0
Arial-Z <amonitux@outlook.com>
2023-02-18 14:10:02 +0100

    fix plex export

--- plex_animes_export.py
+++ plex_animes_export.py
@@ -15,15 +15,20 @@ ANIME_LIBRARY_NAME=environ.get('ANIME_LIBRARY_NAME')
 
 plex = PlexServer(url, token)
 animes = plex.library.section(ANIME_LIBRARY_NAME)
-with open(path.join(basedir, "tmp/plex_animes_export.tsv"), "w") as export_plex:
+with open(path.join(basedir, "tmp/plex_animes_export.tsv"), "w") as export_plex, open(path.join(basedir, "tmp/plex_failed_animes.tsv"), "w") as export_fail:
         for video in animes.search():
                 title = str(video.title)
                 ids = str(video.guids)
-                tvdb = re.search("(?<=tvdb://)(\d+)", ids).group()
-                location = str(video.locations)[2:-2]
-                path = pathlib.PurePath(location)
-                folder = str(path.name)
-                seasons = str(video.seasons())
-                last_season = re.search("(\d+)(?!.*\d)", seasons).group()
-                export=(tvdb + "\t" + title + "\t" + folder + "\t"+ last_season + "\n")
-                export_plex.write(export)
+                tvdbid = re.search("(?<=tvdb://)(\d+)", ids)
+                if ( tvdbid ) :
+                        tvdb = str(tvdbid.group(1))
+                        location = str(video.locations)[2:-2]
+                        path = pathlib.PurePath(location)
+                        folder = str(path.name)
+                        seasons = str(video.seasons())
+                        last_season = re.search("(\d+)(?!.*\d)", seasons).group()
+                        export=(tvdb + "\t" + title + "\t" + folder + "\t"+ last_season + "\n")
+                        export_plex.write(export)
+                else :
+                        export=(title + "no id found" + ids + "\n")
+                        export_fail.write(export)
--- plex_movies_export.py
+++ plex_movies_export.py
@@ -15,16 +15,18 @@ MOVIE_LIBRARY_NAME=environ.get('MOVIE_LIBRARY_NAME')
 
 plex = PlexServer(url, token)
 movies = plex.library.section(MOVIE_LIBRARY_NAME)
-with open(path.join(basedir, "tmp/plex_movies_export.tsv"), "w") as export_plex:
+with open(path.join(basedir, "tmp/plex_movies_export.tsv"), "w") as export_plex, open(path.join(basedir, "tmp/plex_failed_movies.tsv"), "w") as export_fail:
         for video in movies.search():
                 title = str(video.title)
                 ids = str(video.guids)
-                try:
-                        imdb = re.search("(?<=imdb://)(tt\d+)", ids).group()
-                except ValueError:
-                        pass
-                location = str(video.locations)[2:-2]
-                path = pathlib.PurePath(location)
-                folder = str(path.parent.name)
-                export=(imdb + "\t" + title + "\t" + folder + "\n")
-                export_plex.write(export)
+                imdbid = re.search("(?<=imdb://)(tt\d+)", ids)
+                if ( imdbid ) :
+                        imdb = str(imdbid.group(1))
+                        location = str(video.locations)[2:-2]
+                        path = pathlib.PurePath(location)
+                        folder = str(path.parent.name)
+                        export=(imdb + "\t" + title + "\t" + folder + "\n")
+                        export_plex.write(export)
+                else :
+                        export=(title + "no id found" + ids + "\n")
+                        export_fail.write(export)
