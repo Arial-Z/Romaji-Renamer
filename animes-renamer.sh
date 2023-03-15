590d88274dbe18423441ddb1a6bd81ca29f4abd0
Arial-Z <amonitux@outlook.com>
2023-03-15 14:13:55 +0100

    Restore from 1.10

--- animes-renamer.sh
+++ animes-renamer.sh
@@ -15,7 +15,7 @@ then
 	mkdir "$SCRIPT_FOLDER/data"
 else
 	find "$SCRIPT_FOLDER/data/"* -mtime +$MAL_CACHE_TIME -exec rm {} \;					#delete json data if older than 2 days
-	find "$SCRIPT_FOLDER/data/ongoing.tsv" -mmin +720 -exec rm {} \;			#delete ongoing if older than 12h
+	find $SCRIPT_FOLDER/data/ongoing.tsv -mmin +720 -exec rm {} \;			#delete ongoing if older than 12h
 fi
 if [ ! -d "$SCRIPT_FOLDER/tmp" ]										#check if exist and create folder for json data
 then
