12925533e26de5a06bee310a9af3fc2b07ba61ed
Arial-Z <amonitux@outlook.com>
2023-05-18 16:11:33 +0200

    Fix seasonal-dl

--- seasonal-animes-download.sh
+++ seasonal-animes-download.sh
@@ -9,9 +9,9 @@ media_type=animes
 #SCRIPT
 :> "$SCRIPT_FOLDER/data/seasonal.tsv"
 download-anime-id-mapping
-wget -O "$SCRIPT_FOLDER/tmp/this-season.html" "https://myanimelist.net/anime/season"
-current_season=$(awk -v IGNORECASE=1 -v RS='</title' 'RT{gsub(/.*<title[^>]*>/,"");print;exit}' "$SCRIPT_FOLDER/tmp/this-season.html" | awk '{print $1}' | sed -e "s/[^ a-zA-Z]//g" -e 's/ //g')
-current_year=$(awk -v IGNORECASE=1 -v RS='</title' 'RT{gsub(/.*<title[^>]*>/,"");print;exit}' "$SCRIPT_FOLDER/tmp/this-season.html" | awk '{print $2}' | sed -e "s/[^ 0-9]//g" -e 's/ //g')
+wget -O "$SCRIPT_FOLDER/tmp/this-season.html" "https://www.livechart.me/"
+current_season=$(awk -v IGNORECASE=1 -v RS='</title' 'RT{gsub(/.*<title[^>]*>/,"");print;exit}' "$SCRIPT_FOLDER/tmp/this-season.html" | awk '{print $1}')
+current_year=$(awk -v IGNORECASE=1 -v RS='</title' 'RT{gsub(/.*<title[^>]*>/,"");print;exit}' "$SCRIPT_FOLDER/tmp/this-season.html" | awk '{print $2}')
 printf "Current season : %s %s\n" "$current_season" "$current_year"
 curl 'https://graphql.anilist.co/' \
 -X POST \
