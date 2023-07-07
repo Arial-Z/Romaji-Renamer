9608e3b1419572a9d4cda9a419a36bfb6c164d24
Arial-Z <amonitux@outlook.com>
2023-07-07 08:54:37 +0200

    fix S1 score

--- functions.sh
+++ functions.sh
@@ -148,7 +148,7 @@ function less-caps-title () {
 function get-score () {
 	anime_score=0
 	anime_score=$(jq '.data.Media.averageScore' -r "$SCRIPT_FOLDER/data/anilist-$anilist_id.json" | awk '{print $1 / 10 }')
-	if [[ "$anime_score" == "null" ]] || [[ "$anime_score" == '' ]] || [[ "$anime_score" == '0.0' ]]
+	if [[ "$anime_score" == "null" ]] || [[ "$anime_score" == "0.0" ]]
 	then
 		rm "$SCRIPT_FOLDER/data/anilist-$anilist_id.json"
 		get-anilist-infos
@@ -342,11 +342,13 @@ function get-season-infos () {
 					printf "      1:\n        label.sync: %s\n" "$anime_season" >> "$METADATA"
 					if [[ $RATING_SOURCE == "ANILIST" ]]
 					then
-						score=$(get-score)
+						score_season=$(get-score)
 					else
-						score=$(get-mal-score)
+						score_season=$(get-mal-score)
 					fi
-					score=$(printf '%.*f\n' 1 "$score")
+					score_season=$(printf '%.*f\n' 1 "$score_season")
+					anime_season=$(get-animes-season-year)
+					total_score=$(echo | awk -v v1="$score_season" -v v2="$total_score" '{print v1 + v2 }')
 					get-season-poster
 				else
 					anilist_id=$(jq --arg tvdb_id "$tvdb_id" --arg season_number "$season_number" '.[] | select( .tvdb_id == $tvdb_id ) | select( .tvdb_season == $season_number ) | select( .tvdb_epoffset == "0" ) | .anilist_id' -r "$SCRIPT_FOLDER/tmp/list-animes-id.json" | head -n 1)
