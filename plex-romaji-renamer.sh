540a948662ab1813fe7f8483acab149f86103b15
Arial-Z <amonitux@outlook.com>
2023-10-31 18:57:37 +0100

    init

---
+++ plex-romaji-renamer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+RUN_ANIMES_SCRIPTS=0
+RUN_MOVIES_SCRIPTS=0
+export LC_ALL=en_US.UTF-8
+SCRIPT_FOLDER=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+source "$SCRIPT_FOLDER/.env"
+
+if [[ $RUN_ANIMES_SCRIPTS -eq 1 ]]
+then
+	bash "$SCRIPT_FOLDER/animes-renamer.sh"
+fi
+if [[ $RUN_MOVIES_SCRIPTS -eq 1 ]]
+then
+	bash "$SCRIPT_FOLDER/movies-renamer.sh"
+fi
