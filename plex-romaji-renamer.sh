d281028c313cc9b6ec849782a9b91cacdf7db403
Arial-Z <amonitux@outlook.com>
2023-11-01 18:47:48 +0100

    add seasonal in .env & update readme

--- plex-romaji-renamer.sh
+++ plex-romaji-renamer.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-RUN_ANIMES_SCRIPTS=0
-RUN_MOVIES_SCRIPTS=0
+RUN_ANIMES_SCRIPT=0
+RUN_MOVIES_SCRIPT=0
+RUN_SEASONAL_SCRIPT=0
 export LC_ALL=C.UTF-8
 SCRIPT_FOLDER=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 LOG=$LOG_FOLDER/${media_type}_$(date +%Y.%m.%d).log
@@ -20,13 +21,17 @@ fi
 if [ -f "$SCRIPT_FOLDER/config/.env" ]
 then
 	source "$SCRIPT_FOLDER/config/.env"
-	if [[ $RUN_ANIMES_SCRIPTS == "Yes" ]]
+	if [[ $RUN_ANIMES_SCRIPT == "Yes" ]]
 	then
 		bash "$SCRIPT_FOLDER/animes-renamer.sh"
 	fi
-	if [[ $RUN_MOVIES_SCRIPTS == "Yes" ]]
+	if [[ $RUN_MOVIES_SCRIPT == "Yes" ]]
 	then
 		bash "$SCRIPT_FOLDER/movies-renamer.sh"
+	fi
+		if [[ $RUN_SEASONAL_SCRIPT == "Yes" ]]
+	then
+		bash "$SCRIPT_FOLDER/seasonal-animes-download.sh"
 	fi
 else
 	printf "%s - Error no config found\n" "$(date +%H:%M:%S)" | tee -a "$LOG"
