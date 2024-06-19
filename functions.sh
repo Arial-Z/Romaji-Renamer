5b3ac933027ee06651d31baaf80b2ea03d2b71f3
Arial-Z <amonitux@outlook.com>
2024-06-19 17:55:04 +0200

    Fix log

--- functions.sh
+++ functions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #General variables
-LOG=$LOG_FOLDER/PRR_$(date +%Y.%m.%d).log
+LOG=$LOG_FOLDER/RR_$(date +%Y.%m.%d).log
 MATCH_LOG=$LOG_FOLDER/${media_type}-missing-id.log
 
 # functions
